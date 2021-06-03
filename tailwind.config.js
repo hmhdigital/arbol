933cf80031494ef2e53b201965aad02275719c66
Rashad Hawkins <rashad@hellomrhawkins.com>
2021-06-03 05:21:20 -0400

    Added purge rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
-	purge: [],
+	purge: {
+		content: [
+			'./**/*.php',
+			'./views/**/*.twig',
+			'./assets/src/**/*.js'
+		]
+	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
