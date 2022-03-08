7b15ee421bc038d71799c649851b9dac64902627
hmhdigital <rashad@hmh.digital>
2022-03-08 14:02:20 -0500

    Tailwind 3.0 config updates, laravel mix bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-	purge: {
-		content: [
-			'./**/*.php',
-			'./views/**/*.twig',
-			'./assets/src/**/*.js'
-		]
-	},
-	darkMode: false, // or 'media' or 'class'
+	content: [
+		'./views/**/*.twig'
+	],
 	theme: {
 		fontFamily: {
 			'display': [ '', 'sans-serif'],
