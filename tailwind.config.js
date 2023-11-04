c766c9d0ea0c01db7687c5b4dfbce335f1d0d554
hmhdigital <rashad@hmh.digital>
2023-11-04 10:50:36 -0400

    Import fonts and colors from theme.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,46 @@
+/**
+ * See https://tailwindcss.com/docs/configuration for configuration details
+ */
+
+/**
+ * Convert pixels to rems
+ * @param {int} px The pixel value to convert to rems
+ */
+
+
+const fs = require( 'fs' );
+const glob = require('glob');
 const plugin = require('tailwindcss/plugin');
+
+const themeJson = fs.readFileSync( './theme.json' );
+const theme = JSON.parse( themeJson );
+
+// eslint-disable-next-line no-unused-vars
+const rem = px => `${px / 16}rem`;
+
+
+let colors = {};
+theme.settings.color.palette.forEach(color => {
+	colors[color.slug] = color.color;
+});
+
+let fonts = {};
+theme.settings.typography.fontFamilies.forEach(fam => {
+	fonts[fam.slug] = fam.fontFamily.split(',');
+});
+
 
 module.exports = {
 	content: [
+		'./**/*.php',
 		'./views/**/*.twig'
-	],
+	].concat(glob.sync('./*.php')),
+	// have to use glob sync because otherwise base folder becomes tw dependency and infinite loop because of index.asset.php
+	// glob returns array of actual files and this way build folder is definitively ignored
 	theme: {
-		fontFamily: {
-			'display': [ '', 'sans-serif'],
-			'body': [ '', 'sans-serif']
-		},
-		extend: {
-			colors: {
-				'primary': '',
-				'secondary': '',
-				'accent': '',
-			},
+		fontFamily: fonts,
+		extend : {
+			colors: colors,
 			height: {
 				'screen-50': '50vh',
 				'screen-60': '60vh',
@@ -46,8 +72,10 @@ module.exports = {
 	},
 	plugins: [
 		require('@tailwindcss/forms'),
+		// eslint-disable-next-line no-unused-vars
 		plugin(function({ addUtilities, addComponents, e, prefix, config }) {
 			// Add your custom styles here
+			// eslint-disable-next-line no-mixed-spaces-and-tabs
 		  }),
 	],
 };
