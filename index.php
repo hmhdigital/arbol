133eb0a31d1a76446adfd1d2ece806507ee7c7e7
hmhdigital <rashad@hmh.digital>
2023-11-04 10:30:41 -0400

    TODO: Fix featured image on front page error

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ $context['posts'] = Timber::get_posts();
 $templates = array( 'index.twig' );
 
 if ( is_home() ) {
-	$img = wp_get_attachment_image_src(get_post_thumbnail_id( $posts_page ), 'full' );
-    $featured_image = $img[0];
+	// $img = wp_get_attachment_image_src(get_post_thumbnail_id( $posts_page ), 'full' );
+    // $featured_image = $img[0];
 	array_unshift( $templates, 'home.twig' );
 }
 
